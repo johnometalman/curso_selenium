c5efd1bd5d86b364fb010edb3ed851e4a643c3c1
John J. Meza <johnometalmanldu@gmail.com>
2021-07-02 13:13:35 -0500

    test de prueba solo con la página de Platzi.com

--- hello_world.py
+++ hello_world.py
@@ -4,10 +4,15 @@ from selenium import webdriver
 
 class HelloWorld(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path=)
+        #hay que modificar la ruta rependiendo del sistema operativo y donde esté instalado en chromedriver
+        self.driver = webdriver.Chrome(executable_path= r'C:\Users\jm_yg\Dropbox\Educación\MOOCS\selenium\chromedriver.exe')
+        driver = self.driver
+        driver.implicitly_wait(10)
+
 
     def test_hello_world(self):
-        pass
+        driver = self.driver
+        driver.get('https://www.platzi.com')
 
     def tearDown(self):
         self.driver.quit()
