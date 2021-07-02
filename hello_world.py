9012aaed1fea8cf196e0b0aa0ee5abb09ffff8ee
johnometalman <johnometalmanldu@gmail.com>
2021-07-02 12:24:55 -0500

    construcción del archivo en Linux pero debo pasarme a windows

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+import unittest
+from pyunitreport import HTMLTestRunner
+from selenium import webdriver
+
+class HelloWorld(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=)
+
+    def test_hello_world(self):
+        pass
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main(verbosity= 2, testRunner= HTMLTestRunner(output= 'reports', report_name= 'Hello World report', ))
