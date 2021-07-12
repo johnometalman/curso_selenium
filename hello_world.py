d3486e75a2444299b0b263d776f911957d89cf4e
johnometalman <johnometalmanldu@gmail.com>
2021-07-12 18:42:45 -0500

    sin avances

--- hello_world.py
+++ hello_world.py
@@ -7,24 +7,19 @@ class HelloWorld(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        #hay que modificar la ruta rependiendo del sistema operativo y donde esté instalado en chromedriver
-
-        #windows
-        cls.driver = webdriver.Chrome(executable_path= r'C:\Users\jm_yg\Dropbox\Educación\MOOCS\selenium\chromedriver.exe')
-        
-        #linux
-        #cls.driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+        cls.driver = webdriver.Chrome(executable_path= 'usr/bin/chromedriver')
         driver = cls.driver
         driver.implicitly_wait(10)
-        
+
 
 
     def test_hello_world(self):
         driver = self.driver
         driver.get('https://www.platzi.com')
 
-    def tearDown(self):
-        self.driver.quit()
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
 if __name__ == '__main__':
-    unittest.main(verbosity= 2, testRunner= HTMLTestRunner(output= 'reports', report_name= 'Hello World report', ))
+    unittest.main(verbosity= 2, testRunner= HTMLTestRunner(output= 'reports', report_name= 'Hello World report',))
