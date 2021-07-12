d4cf689e9de32e0e3475ec62309518922f4d7a80
John J. Meza <johnometalmanldu@gmail.com>
2021-07-12 16:02:06 -0500

    Si funciona en Windows

--- hello_world.py
+++ hello_world.py
@@ -10,8 +10,10 @@ class HelloWorld(unittest.TestCase):
         #hay que modificar la ruta rependiendo del sistema operativo y donde esté instalado en chromedriver
 
         #windows
-        #cls.driver = webdriver.Chrome(executable_path= r'C:\Users\jm_yg\Dropbox\Educación\MOOCS\selenium\chromedriver.exe')
-        cls.driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+        cls.driver = webdriver.Chrome(executable_path= r'C:\Users\jm_yg\Dropbox\Educación\MOOCS\selenium\chromedriver.exe')
+        
+        #linux
+        #cls.driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
         driver = cls.driver
         driver.implicitly_wait(10)
         
