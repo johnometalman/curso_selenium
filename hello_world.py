ce0c97c7983e7fd555538126cca0caffc98b0401
johnometalman <johnometalmanldu@gmail.com>
2021-07-12 15:44:11 -0500

    Tratando de arreglar el problema de WSL

--- hello_world.py
+++ hello_world.py
@@ -2,24 +2,24 @@ import unittest
 from pyunitreport import HTMLTestRunner
 from selenium import webdriver
 
+
 class HelloWorld(unittest.TestCase):
-    def setUp(self):
+
+    @classmethod
+    def setUpClass(cls):
         #hay que modificar la ruta rependiendo del sistema operativo y donde esté instalado en chromedriver
 
         #windows
-        #self.driver = webdriver.Chrome(executable_path= r'C:\Users\jm_yg\Dropbox\Educación\MOOCS\selenium\chromedriver.exe')
-        
-        self.driver = webdriver.Chrome(executable_path= '/usr/bin/chromedriver')
-        driver = self.driver
+        #cls.driver = webdriver.Chrome(executable_path= r'C:\Users\jm_yg\Dropbox\Educación\MOOCS\selenium\chromedriver.exe')
+        cls.driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+        driver = cls.driver
         driver.implicitly_wait(10)
+        
 
 
     def test_hello_world(self):
         driver = self.driver
         driver.get('https://www.platzi.com')
-
-    def test_edundipia(self):
-        driver = self.driver.get('https://www.edundipia.com')
 
     def tearDown(self):
         self.driver.quit()
